2ad46a31c3df8245526f86793503411c8afecee7
Akbarkhon Amirkhonov <8295502+amirkhonov@users.noreply.github.com>
2022-06-07 07:58:32 +0500

    Add reverse string

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	rns := []rune(input) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	// return the reversed string.
+	return string(rns)
 }
